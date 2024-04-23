2cff6ade8fff7ac0e8d3c4219f97220230bf5546
Pradeep-Prady <pradeepprady005@gmail.com>
2024-04-23 15:20:14 +0530

    check

--- backend/app.js
+++ backend/app.js
@@ -30,12 +30,12 @@ app.use("/api/v1/", auth);
 app.use("/api/v1/", award);
 app.use("/api/v1/", testimonial);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 app.use(errorMiddleware);
 
