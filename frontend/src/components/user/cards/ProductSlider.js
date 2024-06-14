d98ebc1a55df8f7f745ee15591a7db3e0d03d468
Pradeep-Prady <pradeepprady005@gmail.com>
2024-06-14 16:28:25 +0530

    slider error fix

--- frontend/src/components/user/cards/ProductSlider.js
+++ frontend/src/components/user/cards/ProductSlider.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "../cards/styles/swiper-custom.css"; // Assuming you have custom styles here
+
 export default function ProductSlider({ images }) {
   return (
-    <div className="w-full relative  h-auto sm:h-[500px]  flex items-center justify-center   px-5 sm:px-10 md:px-32 overflow-hidden">
-      <div className="h-full w-auto relative   sm:w-[60%]">
+    <div className="w-full relative h-auto sm:h-[500px] flex items-center justify-center px-5 sm:px-10 md:px-32 overflow-hidden">
+      <div className="h-full w-full relative sm:w-[60%]">
         <Swiper
-          // className="h-auto  w-full sm:w-[60%]"
           className="h-full w-full"
           spaceBetween={30}
           centeredSlides={true}
@@ -24,7 +25,6 @@ export default function ProductSlider({ images }) {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
           }}
-          // navigation={true}
         >
           {images?.map((imageSrc, index) => (
             <SwiperSlide key={index}>
@@ -37,8 +37,8 @@ export default function ProductSlider({ images }) {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="swiper-button-prev p-2 sm:p-4 md:p-5   !text-myrose  z-10"></div>
-        <div className="swiper-button-next p-2 sm:p-4 md:p-5   !text-myrose absolute right-0 z-10"></div>
+        <div className="swiper-button-prev p-2 sm:p-4 md:p-5 absolute z-10"></div>
+        <div className="swiper-button-next p-2 sm:p-4 md:p-5 absolute right-0 z-10"></div>
       </div>
     </div>
   );
